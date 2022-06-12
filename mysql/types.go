5012a6330a0c6dd83d7a0ad2fef7e0a8c79bcde5
tingtt <ting.taku@gmail.com>
2022-06-12 11:07:31 +0900

    [add] DocumetのPOST/PUTを実装　

--- mysql/types.go
+++ mysql/types.go
@@ -28,3 +28,30 @@ func (p *PatchNullJSONString) UnmarshalJSON(data []byte) error {
 	p.String = &tmpP
 	return nil
 }
+
+// null, null => jsonにキーが存在しない
+// ポインター, null => jsonにキーが存在し、値がnull
+// ポインター, ポインター => jsonにキーが存在し、値がnull以外
+type PatchNullJSONSlice[T any] struct {
+	Slice **[]T `validate:"omitempty,dive"`
+}
+
+func (p *PatchNullJSONSlice[T]) UnmarshalJSON(data []byte) error {
+	// jsonにキーが存在する場合にこの関数が呼び出される
+	var valueP *[]T = nil
+	if string(data) == "null" {
+		// key exists and value is null
+		p.Slice = &valueP
+		return nil
+	}
+
+	var tmp []T
+	tmpP := &tmp
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		// typeエラー
+		return err
+	}
+	// valid value
+	p.Slice = &tmpP
+	return nil
+}

2f42217bcb324ec849eca854a704cd63b6e28b40
tingtt <ting.taku@gmail.com>
2022-06-13 10:17:29 +0900

    [update] 再利用しないステートメントを除外

--- mysql/utils.go
+++ mysql/utils.go
@@ -10,13 +10,7 @@ func Read(queryStr string, args ...any) (rows *sql.Rows, err error) {
 	}
 	defer db.Close()
 
-	stmtOut, err := db.Prepare(queryStr)
-	if err != nil {
-		return
-	}
-	defer stmtOut.Close()
-
-	return stmtOut.Query(args...)
+	return db.Query(queryStr, args...)
 }
 
 // INSERT, UPDATE, DELETE
@@ -26,12 +20,8 @@ func Write(queryStr string, args ...any) (result sql.Result, err error) {
 		return
 	}
 	defer db.Close()
-	stmtIns, err := db.Prepare(queryStr)
-	if err != nil {
-		return
-	}
-	defer stmtIns.Close()
-	return stmtIns.Exec(args...)
+
+	return db.Exec(queryStr, args...)
 }
 
 // SELECT
@@ -41,10 +31,5 @@ func TxRead(tx *sql.Tx, queryStr string, args ...any) (rows *sql.Rows, err error
 
 // INSERT, UPDATE, DELETE
 func TxWrite(tx *sql.Tx, queryStr string, args ...any) (result sql.Result, err error) {
-	stmtIns, err := tx.Prepare(queryStr)
-	if err != nil {
-		return
-	}
-	defer stmtIns.Close()
-	return stmtIns.Exec(args...)
+	return tx.Exec(queryStr, args...)
 }

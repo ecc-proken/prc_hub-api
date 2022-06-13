fcd3fb5a9182d850a65c3bbd7c9923a0922b8089
tingtt <ting.taku@gmail.com>
2022-06-13 10:22:12 +0900

    [update] 参加登録解除のトランザクション実装

--- events/unparticipate.go
+++ events/unparticipate.go
@@ -1,22 +1,47 @@
 package events
 
-import "prc_hub-api/mysql"
+import (
+	"prc_hub-api/mysql"
+)
 
 func Unparticipate(datetimeId uint64, userId uint64) (notFound bool, err error) {
+	// トランザクション開始
+	tx, err := mysql.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+
 	// 参加登録情報を取得
-	r, err := mysql.Read("SELECT true FROM event_participates WHERE event_datetime_id = ? AND user_id = ?", datetimeId, userId)
+	r, err := mysql.TxRead(tx, "SELECT true FROM event_participates WHERE event_datetime_id = ? AND user_id = ?", datetimeId, userId)
 	if err != nil {
 		return
 	}
 	defer r.Close()
-
 	// 登録済みか確認
 	if !r.Next() {
 		notFound = true
 		return
 	}
+	err = r.Close()
+	if err != nil {
+		return
+	}
 
 	// 削除
-	_, err = mysql.Write("DELETE FROM event_participates WHERE event_datetime_id = ? AND user_id = ?", datetimeId, userId)
+	_, err = mysql.TxWrite(tx, "DELETE FROM event_participates WHERE event_datetime_id = ? AND user_id = ?", datetimeId, userId)
+	if err != nil {
+		return
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return
+	}
+
 	return
 }
